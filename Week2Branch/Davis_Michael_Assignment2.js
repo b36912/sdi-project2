e5f626f0b63c05c2169b00e4cb983bc4cfc11fb0
Michael Davis <mdavisii@fullsail.edu>
2014-03-10 23:01:23 -0400

    Update and rename rename-me.js to Davis_Michael_Assignment2.js
    Added variables

---
+++ Week2Branch/Davis_Michael_Assignment2.js
@@ -0,0 +1,21 @@
+//alert("JavaScript works!");
+
+//Michael Davis
+//SDI 1403 SECTION 01
+//March 13, 2014
+
+/*
+
+
+*/
+
+//Variables (Internal Storage)
+
+var davisFamily        = "Davis Family";
+var driveStart         = "Philadelphia, PA";
+var driveDestination   = "to \"Houston, Tx\"";
+var familyMembers      = 4;
+var needSleep          = true;
+var numberHotels       = 6;
+var hotelCity          = "Atlanta";
+var hungry;

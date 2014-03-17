51e9d223aca228bd8c000826361b254728eac99c
Michael Davis <mdavisii@fullsail.edu>
2014-03-17 07:04:41 -0400

    Update rename-me.js

--- Davis_Michael.Assignment3/rename-me.js
+++ Davis_Michael.Assignment3/rename-me.js
@@ -1,1 +1,38 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+/*
+Michael Davis
+SDI 1403
+Assignment 3
+*/
+
+//Global Variables
+
+//Arrays
+
+//Jsons
+//list of misc 
+  var
+  var
+  var
+  var
+  var
+  var
+  
+console.log();
+
+
+() {}
+
+() {}
+
+() []
+
+() []
+
+confirm()
+
+return()
+
+prompt()
+

998239ddb3b40992708886ddaecc175c38528602
Michael Davis <mdavisii@fullsail.edu>
2014-03-17 23:27:32 -0400

    Create json3p.js

---
+++ Davis_Michael.Assignment3/json3p.js
@@ -0,0 +1,24 @@
+// json
+var jsonData = {
+
+    "familyMembers":
+        [
+            {
+                "memberFirst": "Dad",
+                "memberName":  "Mike",
+                "memberAge":    35
+            },
+            {
+                "memberFirst": "Mom",
+                
+            },
+            {
+                
+            },
+            {
+                
+            }
+        
+        ]
+    
+};

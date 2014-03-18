f29c5dee1654b15ca96c6ea71bdbaa78b83c39a0
Michael Davis <mdavisii@fullsail.edu>
2014-03-17 23:44:48 -0400

    Update json3p.js
    Family Data

--- Davis_Michael.Assignment3/json3p.js
+++ Davis_Michael.Assignment3/json3p.js
@@ -1,22 +1,40 @@
 // json
-var jsonData = {
+var tripData = {
 
     "familyMembers":
         [
             {
-                "memberFirst": "Dad",
-                "memberName":  "Mike",
-                "memberAge":    35
+                "memberName"    :  "Dad",
+                "memberAge "    :   40
+                "memberLChoice" :  "Panera Bread"'
+                "memberLunch"   :  "turkey panini sandwich with kettle chips and water",
+                "memberCost"    :   8
+                "memberWallet"  :   130
             },
             {
-                "memberFirst": "Mom",
+                "memberName"    :  "Mom",
+                "memberAge "    :   39
+                "memberLChoice" :  "Panera Bread",
+                "memberLunch"   :  "chicken panini sandwich with an apple and water",
+                "memberCost"    :   8
+                "memberWallet"  :   128
                 
             },
             {
-                
+               "memberName"    :  "Son",
+                "memberAge "    :   12
+                "memberLChoice" :  "Chick Fi La",
+                "memberLunch"   :  "turkey panini sandwich with kettle chips and a water",
+                "memberCost"    :   7.25
+                "memberWallet"  :   0 
             },
             {
-                
+                "memberName"    :  "Daughter",
+                "memberAge "    :   10
+                "memberLChoice" :  "Chick Fi La",
+                "memberLunch"   :  "chicken sandwich with a mixed fruit cup and a lemonade",
+                "memberCost"    :   6.25
+                "memberWallet"  :   0
             }
         
         ]

e0b23c58cafbba01f79142750b301d87907fde1f
Michael Davis <mdavisii@fullsail.edu>
2014-03-18 22:37:57 -0400

    Update json3p.js

--- Davis_Michael.Assignment3/json3p.js
+++ Davis_Michael.Assignment3/json3p.js
@@ -1,3 +1,4 @@
+
 // json
 var tripData =
 {
@@ -5,37 +6,37 @@ var tripData =
     "familyMembers":
         [
             {
-                "memberName"    :  "Dad",
-                "memberAge "    :   40
-                "memberLChoice" :  "Panera Bread"'
-                "memberLunch"   :  "turkey panini sandwich with kettle chips and water",
-                "memberCost"    :   8
-                "memberWallet"  :   130
+                "memberName"    : "Dad",
+                "memberAge"     : 40,
+                "memberLunchC"  : "Panera Bread",
+                "memberLunch"   : "turkey panini sandwich with kettle chips and water",
+                "memberCost"    : 8,
+                "memberWallet"  : 130,
             },
             {
-                "memberName"    :  "Mom",
-                "memberAge "    :   39
-                "memberLChoice" :  "Panera Bread",
-                "memberLunch"   :  "chicken panini sandwich with an apple and water",
-                "memberCost"    :   8
-                "memberWallet"  :   128
+                "memberName"    : "Mom",
+                "memberAge"     : 39,
+                "memberLChoice" : "Panera Bread",
+                "memberLunch"   : "chicken panini sandwich with an apple and water",
+                "memberCost"    : 8,
+                "memberWallet"  : 128,
                 
             },
             {
-                "memberName"    :  "Son",
-                "memberAge "    :   12
-                "memberLChoice" :  "Chick Fi La",
-                "memberLunch"   :  "turkey panini sandwich with kettle chips and a water",
-                "memberCost"    :   7.25
-                "memberWallet"  :   0 
+                "memberName"    : "Son",
+                "memberAge"     : 12,
+                "memberLChoice" : "Chick Fi La",
+                "memberLunch"   : "turkey panini sandwich with kettle chips and a water",
+                "memberCost"    : 7.25,
+                "memberWallet"  : 0 ,
             },
             {
-                "memberName"    :  "Daughter",
-                "memberAge "    :   10
-                "memberLChoice" :  "Chick Fi La",
-                "memberLunch"   :  "chicken sandwich with a mixed fruit cup and a lemonade",
-                "memberCost"    :   6.25
-                "memberWallet"  :   0
+                "memberName"    : "Daughter",
+                "memberAge"     : 10,
+                "memberLChoice" : "Chick Fi La",
+                "memberLunch"   : "chicken sandwich with a mixed fruit cup and a lemonade",
+                "memberCost"    : 6.25,
+                "memberWallet"  : 0,
             }
         
         ]
@@ -44,25 +45,27 @@ var tripData =
 var hotelData =
 {
 
+    "hotels":
+        [
             {
                 "hotel"         :  "Comfort Inn",
-                "hotelCost"     :   128
+                "hotelCost"     :   128,
             },
             {
                 "hotel"         :  "Hyatt",
-                "hotelCost"     :   135
+                "hotelCost"     :   135,
             },
             {
                 "hotel"         :  "Hilton Garden Inn",
-                "hotelCost"     :   140
+                "hotelCost"     :   140,
             },
             {
                 "hotel"         :  "Sheraton",
-                "hotelCost"     :   156
+                "hotelCost"     :   156,
             },
             {
                 "hotel"         :  "Embassy Suites",
-                "hotelCost"     :   140
+                "hotelCost"     :   140,
             }
             
         ]

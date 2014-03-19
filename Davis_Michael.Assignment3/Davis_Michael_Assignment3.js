3029d5fc419e1371ec1c6095c856b6279aed948f
Michael Davis <mdavisii@fullsail.edu>
2014-03-19 02:26:33 -0400

    Update Davis_Michael_Assignment3.js

--- Davis_Michael.Assignment3/Davis_Michael_Assignment3.js
+++ Davis_Michael.Assignment3/Davis_Michael_Assignment3.js
@@ -6,33 +6,52 @@ SDI 1403
 Assignment 3
 */
 
+//Json = Trip data describes family members and hotels and their costs.
+var tripData  = ["familyMembers"];
+var hotelData = ["hotels"];
+
 //Global Variables
+var theFam  = "Davis Family";
+var start   = "Philly, PA";
+var finish  = "Houston, TX";
+var hours   =  25
+var nonStop =  false
 
-//Arrays
-
-//Jsons
-//list of misc 
-  var
-  var
-  var
-  var
-  var
-  var
-  
-console.log();
+//Alert
 
 
-() {}
 
-() {}
+//
+var goNow;
 
-() []
+for (goNow = 2; goNow >= 0; goNow-- )
+{
+    console.log(goNow + " " +tripData[goNow]);
+    console.log(hotelData[goNow]);
+}
 
-() []
 
-confirm()
 
-return()
 
-prompt()
+//If Statements functions
+var hotels;
+    var hotels = prompt("The family made it to a hotel, are their any rooms available?"); 
+        console.log(hotels + ", there are plenty of vacancies available here at Domino Hotel.");
 
+    if (needSleep === true) {
+
+        var hotelCost = prompt("How much does it cost for a non smoking room with 2 beds?");
+            console.log(hotelCost);
+            hotelCost = parseInt(hotelCost);
+            console.log(hotelCost + ", this includes tax as well as continental breakfast and wifi.");
+   if (hotelCost < 200) {
+        console.log("We'll take the room.");
+   } else {
+        console.log("Its a bit pricey, are there any corporate discounts available?");
+   }; 
+} else {
+    
+    console.log("Thanks anyway.");
+};
+
+console.log("The family found a hotel to stay in for the night");

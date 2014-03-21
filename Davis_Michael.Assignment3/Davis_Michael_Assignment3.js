69b4d73e97d8c8abf5727a0e52511c3a25bb3f0e
Michael Davis <mdavisii@fullsail.edu>
2014-03-20 20:48:51 -0400

    Update Davis_Michael_Assignment3.js

--- Davis_Michael.Assignment3/Davis_Michael_Assignment3.js
+++ Davis_Michael.Assignment3/Davis_Michael_Assignment3.js
@@ -6,67 +6,108 @@ SDI 1403
 Assignment 3
 */
 
+
+    
 //Json = Trip data describes family members and hotels and their costs.
-var tripData  = ["familyMembers"];
-var hotelData = ["hotels"];
-
+    var tripData  = ["familyMembers", "hotels"];
+    var hotelData = ["hotels"];
+    
 //Global Variables
-var theFam  = "Davis Family";
-var start   = "Philly, PA";
-var finish  = "Houston, TX";
-var hours   =  25
-var nonStop =  false
+    var theFam  = "Davis Family";
+    var start   = "Philly, PA";
+    var finish  = "Houston, TX";
+    var hours   =  25
+    var nonStop =  false
+    var lunch   =  ["memberLChoice", "memberLunch", "memberCost"];
+    var sleep   =  ["hotel", "hotelCost"];
+    var stopTrip=  true
 
-//Alert
 
 
+var getReady = function(name)
 
-//
-var lunch = function (name, eat, lunch) {
-    if ()
+{        
+       
+           console.log("Were about to leave " + name + "!");
+            
+        };
+        getReady("Tinasha");
+        
+        
+
+//Start the car
+
+while (hours || nonStop)
+        
+        if(hours > nonStop) {
+            
+          console.log("We're driving for " + hours + " hours to " + finish + "!");
+            
+        }else 
+        {
+          timeGo(hours > stopTrip);
+          console.log(timeGo);
+          
+        };
+
+//lunchTime
+var posHours = 48
+
+var lunch = number(prompt ("Enter hours driven so far.", 5)
+    {
+    if (possHours - 5)
     { 
       
-    console.log.();
+        prompt("Is it time to eat?",yes);
     
     } else
     {
      
-    console.log();
+    console.log("We're still driving.");
       
     }
-  };
+};
   lunch()
   
-var goNow;
-
-for (goNow = 2; goNow >= 0; goNow-- )
-{
-    console.log(goNow + " " +tripData[goNow]);
-    console.log(hotelData[goNow]);
-}
-
-
-
 
 //If Statements functions
 var hotels;
     var hotels = prompt("The family made it to a hotel, are their any rooms available?"); 
-        console.log(hotels + ", there are plenty of vacancies available here at Domino Hotel.");
+            console.log(hotels + ", there are plenty of vacancies available here at Domino Hotel.");
 
     if (needSleep === true) {
 
-        var hotelCost = prompt("How much does it cost for a non smoking room with 2 beds?");
+        var hotelCost = promp("How much does it cost for a non smoking room with 2 beds?");
             console.log(hotelCost);
             hotelCost = parseInt(hotelCost);
             console.log(hotelCost + ", this includes tax as well as continental breakfast and wifi.");
+   
    if (hotelCost < 200) {
-        console.log("We'll take the room.");
+            console.log("We'll take the room.");
    } else {
-        console.log("Its a bit pricey, are there any corporate discounts available?");
+            console.log("Its a bit pricey, are there any corporate discounts available?");
    }; 
 } else {
     
-    console.log("Thanks anyway.");
+            console.log("Thanks anyway.");
 };
 
-console.log("The family found a hotel to stay in for the night");
+            console.log("The family found a hotel to stay in for the night");
+
+
+
+//for loop
+
+var word = "";
+    for (i = 1; i <= 10; i++) {
+    
+    //statement
+    
+        for (j = 1; i <= 10; j++) {
+        
+        //statement
+        
+        }
+    
+    };
+    word(word);

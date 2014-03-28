df261bf5382c27c6414c00780ffcfe860e1e45a4
Michael Davis <mdavisii@fullsail.edu>
2014-03-27 23:52:12 -0400

    Update Davis_Michaael_Assignment4.js

--- Davis_Michael_Assignment4/Davis_Michaael_Assignment4.js
+++ Davis_Michael_Assignment4/Davis_Michaael_Assignment4.js
@@ -8,10 +8,16 @@ PROJECT 4
 */
 
         
+//var lilWords = prompt(“Type a statement ”,” eugene is a dad”);
+
+//console.log(“The first characters of the lilWords“ + lilWords);
 //1. telephone
 var numReturn;
 /*
 Strings
+
+this  function uses if statements and for loops
+uses comparisons to determine true or false
 */
 var checktelNum = function (num) {
 
@@ -28,14 +34,7 @@ var checktelNum = function (num) {
             console.log("The telphone number is false.");
         }
     };
-};
-
-// Check String Function
-var phoneNum = function () {
-                
-};
-
-// Main code        
+};       
 numReturn = checktelNum("215-287-3163");
 
 
@@ -44,6 +43,8 @@ numReturn = checktelNum("215-287-3163");
 var emailReturn;
 /*
 Strings
+this  function uses if statements and for loops
+uses comparisons to determine true or false
 */
 var checkeMail = function addres(email) {
 
@@ -60,20 +61,18 @@ var eMailLength = email.length;
             console.log("The email address is true.");
         }
     };
-};
-
-// Check String Function
-var eMailAd = function () {
-                
-};
-
-// Main code        
+};      
 emailReturn = checkeMail("mdavisii@fullsail.edu");
 
+
 //3. URL
+
 var wSiteReturn;
+
 /*
 Strings
+this  function uses if statements and for loops
+uses comparisons to determine true or false
 */
 var checkwebS = function web(site) {
 
@@ -91,11 +90,38 @@ var wSiteLength = site.length;
         }
     };
 };
-
-// Check String Function
-var uWebSite = function () {
-                
-};
-
-// Main code        
 wSiteReturn = checkwebS("www.fullsail.com");
+
+
+//4. numbers math function
+var itemCost = 11.25;
+var finalCost = 11.25 * 2.09;
+var cost = function(money) {
+    if(11+2) {
+    console.log (money.toFixed(finalCost));
+    }
+};
+cost(finalCost);
+
+//5.I used the parse to turn it from  string to a number
+var strNumber = function (num) {
+    console.log(num);
+};
+strNumber(5);
+
+var number = parseInt("10");
+console.log(parseInt(number));
+
+
+
+//6. comparisons and operators, this is also for assignment 2
+var fuzzyNumber = function (num,comNumber,percent) {
+        var percentage = (num / comNumber) * 100;
+        if ((num >= comNumber && percentage >= percent) || (num < comNumber && percentage < percent)) {
+                return false;
+        } else {
+                return true;
+        };
+        };
+        console.log(fuzzyNumber(1,27,54));
+        console.log(fuzzyNumber(54,1,27));
